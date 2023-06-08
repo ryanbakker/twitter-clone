25e06bb6cf75db5375750936efba25db7176459b
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-06-08 15:44:36 +1200

    Tweet Component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,20 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import { TweetType } from "../types";
+import { Entypo, EvilIcons } from "@expo/vector-icons";
+
+type IconButtonProps = {
+  icon: React.ComponentProps<typeof EvilIcons>["name"];
+  text?: string | number;
+};
+
+const IconButton = ({ icon, text }: IconButtonProps) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <EvilIcons name={icon} size={22} color="gray" />
+      <Text style={{ fontSize: 12, color: "gray" }}>{text}</Text>
+    </View>
+  );
+};
 
 type TweetProps = {
   tweet: TweetType;
@@ -11,12 +26,33 @@ const Tweet = ({ tweet }: TweetProps) => {
       <Image src={tweet.user.image} style={styles.userImage} />
 
       <View style={styles.mainContainer}>
-        <Text style={styles.name}>{tweet.user.name}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>@{tweet.user.username} • 2h</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={17}
+            color="lightgray"
+            style={{ marginLeft: "auto" }}
+          />
+        </View>
+
         <Text style={styles.content}>{tweet.content}</Text>
+
+        {tweet.image && <Image src={tweet.image} style={styles.image} />}
+
+        <View style={styles.footer}>
+          <IconButton icon="comment" text={tweet.numberOfComments} />
+          <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+          <IconButton icon="heart" text={tweet.numberOfLikes} />
+          <IconButton icon="chart" text={tweet.impressions || 0} />
+          <IconButton icon="share-apple" />
+        </View>
       </View>
     </View>
   );
 };
+1;
 
 const styles = StyleSheet.create({
   container: {
@@ -38,9 +74,25 @@ const styles = StyleSheet.create({
   name: {
     fontWeight: "600",
   },
+  username: {
+    color: "gray",
+    marginLeft: 5,
+  },
   content: {
     lineHeight: 20,
     marginTop: 5,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 16 / 10,
+    marginTop: 10,
+    marginBottom: 5,
+    borderRadius: 15,
+  },
+  footer: {
+    flexDirection: "row",
+    marginVertical: 5,
+    justifyContent: "space-between",
   },
 });
 
