eb7dfe7522563fadcbaffb8a4bd45a063145e951
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-06-08 20:09:29 +1200

    Drawer Navigation Config

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -5,6 +5,20 @@ const DrawerNavigator = createDrawerNavigator().Navigator;
 
 const Drawer = withLayoutContext(DrawerNavigator);
 
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "(tabs)",
+};
+
 export default function DrawerLayout() {
-  return <Drawer />;
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="(tabs)"
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Drawer.Screen name="bookmarks" options={{ title: "Bookmarks" }} />
+      <Drawer.Screen name="twitter-blue" options={{ title: "Twitter Blue" }} />
+    </Drawer>
+  );
 }
