cbaaeda40cef2d596724382251fa38d2ee4fe886
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-06-08 17:44:32 +1200

    New tweet page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Pressable } from "react-native";
 import Tweet from "../../components/Tweet";
 import tweets from "../../assets/data/tweets";
+import { Entypo } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 export default function TabOneScreen() {
   return (
@@ -10,6 +12,12 @@ export default function TabOneScreen() {
           data={tweets}
           renderItem={({ item }) => <Tweet tweet={item} />}
         />
+
+        <Pressable style={styles.floatingButton}>
+          <Link href="/new-tweet" asChild>
+            <Entypo name="plus" size={24} color="white" />
+          </Link>
+        </Pressable>
       </View>
     </>
   );
@@ -19,5 +27,23 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: "white",
+  },
+  floatingButton: {
+    backgroundColor: "#1C9BF0",
+    padding: 14,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 10,
+    bottom: 11,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
